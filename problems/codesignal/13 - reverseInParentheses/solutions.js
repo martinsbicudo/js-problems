203976bed52b95923a03b61ce6ed650bed3a4148
Victor Bicudo <victor_bicudo@outlook.com>
2023-02-08 12:20:14 -0300

    feat: solve codesign problem 13

--- problems/codesignal/13 - reverseInParentheses/solutions.js
+++ problems/codesignal/13 - reverseInParentheses/solutions.js
@@ -1,3 +1,29 @@
-function solution(inputString) {
+// it helps to get deeper substring between characters
+const regex = /\(([^()]*)\)/g
 
+function solution(string) {
+    let active = true
+    let result = string
+
+    while (active) {
+        let substring = result.match(regex)?.[0]
+        
+        if (substring) {
+            let substrinsReplace = substring.replace(/\(|\)/g, '')
+            substrinsReplace = [...substrinsReplace].reverse().join('')
+            result = result.replace(substring, substrinsReplace)
+        } else active = false
+    }
+
+    return result
+}
+
+function solution2(string) {
+    let result = string
+
+    while (result.includes('(')) {
+        result = result.replace(regex, (found, string) => [...string].reverse().join(''))
+    }
+
+    return result
 }
