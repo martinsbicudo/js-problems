db1bd71e4348da224a43efb6e648cc30b234d9ac
Victor Bicudo <victor_bicudo@outlook.com>
2023-02-08 23:16:56 -0300

    chore: config pre-commit with husky feat: solve codesign problem 18

--- problems/codesignal/18 - palindromeRearranging/solutions.js
+++ problems/codesignal/18 - palindromeRearranging/solutions.js
@@ -1,6 +1,19 @@
-// WIP
-function solution(inputArray) {
+function solution(string) {
+  let primeNumbers = 0
+
+  for (letter of [...new Set(string)]) {
+    const repeats = string.match(new RegExp(letter, 'g')).length
+    if (repeats % 2 === 1) primeNumbers++
+    if (primeNumbers > 1) return false
+  }
+
   return true
 }
 
+function solution2(string) {
+  return [...new Set(string)].filter((letter) =>
+    string.match(new RegExp(letter, 'g')).length % 2 === 1).length < 2
+}
+
 module.exports.solution = solution
+module.exports.solution2 = solution2
--- problems/codesignal/18 - palindromeRearranging/test.js
+++ problems/codesignal/18 - palindromeRearranging/test.js
@@ -1,7 +1,72 @@
 const test = require('ava')
-const { solution } = require('./solutions')
+const { solution, solution2 } = require('./solutions')
 
 test('[codesign][palindromeRearranging] should return true', t => {
   const result = solution('aabb')
+  const result2 = solution('aabb')
   t.truthy(result)
+  t.truthy(result2)
+})
+
+test('[codesign][palindromeRearranging] should return true | 2', t => {
+  const result = solution('abbcabb')
+  const result2 = solution('abbcabb')
+  t.truthy(result)
+  t.truthy(result2)
+})
+
+test('[codesign][palindromeRearranging] should return true | 3', t => {
+  const result = solution('zyyzzzzz')
+  const result2 = solution('zyyzzzzz')
+  t.truthy(result)
+  t.truthy(result2)
+})
+
+test('[codesign][palindromeRearranging] should return true | 4', t => {
+  const result = solution('z')
+  const result2 = solution('z')
+  t.truthy(result)
+  t.truthy(result2)
+})
+
+test('[codesign][palindromeRearranging] should return true | 5', t => {
+  const result = solution('zaa')
+  const result2 = solution('zaa')
+  t.truthy(result)
+  t.truthy(result2)
+})
+
+test('[codesign][palindromeRearranging] should return false', t => {
+  const result = solution('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaabc')
+  const result2 = solution('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaabc')
+  t.falsy(result)
+  t.falsy(result2)
+})
+
+test('[codesign][palindromeRearranging] should return false | 2', t => {
+  const result = solution('abca')
+  const result2 = solution('abca')
+  t.falsy(result)
+  t.falsy(result2)
+})
+
+test('[codesign][palindromeRearranging] should return false | 3', t => {
+  const result = solution('abcad')
+  const result2 = solution('abcad')
+  t.falsy(result)
+  t.falsy(result2)
+})
+
+test('[codesign][palindromeRearranging] should return false | 4', t => {
+  const result = solution('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbccccaaaaaaaaaaaaa')
+  const result2 = solution('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbccccaaaaaaaaaaaaa')
+  t.falsy(result)
+  t.falsy(result2)
+})
+
+test('[codesign][palindromeRearranging] should return false | 5', t => {
+  const result = solution('abdhuierf')
+  const result2 = solution('abdhuierf')
+  t.falsy(result)
+  t.falsy(result2)
 })
